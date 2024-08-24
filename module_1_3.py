4a89b43d2451e5835d40a0448b91eeaeecf29edb
Katrin <Katinka.Ilicheva@yandex.ru>
2024-08-24 00:42:05 -0700

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+#Переменная со значением имени
+name = "Екатерина"
+print("Name:" + name)
+#Переменная со значением возраста
+age = 24
+print("Age:"+ str(age))
+#Перезаписать значение переменной
+age = (age + 2)
+print("New Age:"+ str(age))
+#Ещё одна переменная
+is_student = True
+print("Is Student:" + str(is_student))
